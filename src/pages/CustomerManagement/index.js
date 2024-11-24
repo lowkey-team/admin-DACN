68a1fb446864ced57dfed3a51f4566153262acdf
XBinhs17 <nguyenbinh17062003@gmai.com>
2024-11-24 23:14:46 +0700

    50% customer management page

---
+++ src/pages/CustomerManagement/index.js
@@ -0,0 +1,17 @@
+import classNames from "classnames/bind";
+
+import styles from './CustomerManagement.module.scss'
+
+const cx = classNames.bind(styles);
+
+function CustomerManagement() {
+    return ( 
+        <div className={cx('wrapper')}>
+            <div >
+
+            </div>
+        </div>
+     );
+}
+
+export default CustomerManagement;
