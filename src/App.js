a316540eb36c6bfe2092ba6fe9468c63685f1b54
XBinhs17 <nguyenbinh17062003@gmai.com>
2024-11-28 00:14:55 +0700

    login page and fix layout route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './Routes';
+import { Fragment } from 'react';
+import { DefaultLayout } from './components/Layout';
 
 function App() {
     return (
         <div className="App">
-            <Routes>
+           <Routes>
                 {publicRoutes.map((route, index) => {
                     const Page = route.component;
-                    return <Route key={index} path={route.path} element={<Page />} />;
+                    const Layout =
+                        route.layout === null
+                            ? Fragment 
+                            : route.layout || DefaultLayout; 
+
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page />
+                                </Layout>
+                            }
+                        />
+                    );
                 })}
             </Routes>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Layout from './components/Layout';
+import Layout from './components/Layout/DefaultLayout';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Router>
-            <Layout>
                 <App />
-            </Layout>
         </Router>
     </React.StrictMode>,
 );
